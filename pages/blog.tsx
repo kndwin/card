2c54d078178eeb9f0751216b76b57b6bdbd0da97
Kevin Nguyen <me@kndwin.dev>
2021-09-02 20:32:23 +1000

    ➖ - removed Cols

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,4 +1,4 @@
-import { Card, Spacer, Col, Row, Link, Text } from "@geist-ui/react";
+import { Card, Link, Text } from "@geist-ui/react";
 import { Layout } from "components";
 import { GetStaticProps } from "next";
 import { getSortedPostsData } from "utils/posts";
@@ -14,26 +14,30 @@ type AllPostData = {
 export default function BlogPage({ allPostsData }: AllPostData) {
   return (
     <Layout>
+			<div className='flex flex-col items-center justify-center w-full h-full'>
       {allPostsData.map(({ id, date, title }) => (
-        <Card key={id} style={{ margin: "1em 0 2em 0" }} hoverable>
-          <Row style={{ alignItems: "center" }}>
-            <Col>
-              <Link
-                style={{ fontWeight: "bold", fontSize: "1.5em" }}
-                block
-                href={`/posts/${id}`}
-              >
-                {title}
-              </Link>
-            </Col>
-            <Col span={8}>
-              <Text style={{ fontSize: "1.3em" }}>
-                {new Date(date).toDateString()}
-              </Text>
-            </Col>
-          </Row>
-        </Card>
+        <div
+          style={{
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%",
+            maxWidth: "40em",
+          }}
+        >
+          <Link
+            style={{ fontWeight: "bold", fontSize: "1.5em" }}
+            block
+            href={`/posts/${id}`}
+          >
+            {title}
+          </Link>
+          <Text style={{ fontSize: "1.3em" }}>
+            {new Date(date).toDateString()}
+          </Text>
+        </div>
       ))}
+			</div>
     </Layout>
   );
 }
